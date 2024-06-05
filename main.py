def099f9ec3eb1c0e95c4079ffe3e50f27628a6a
greg <g.kanavenko@gmail.com>
2024-06-05 16:32:17 +0300

    the code organized to consists of parsing csv and parquet

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 
 
-def routine():
+DATA_BASE = '/home/greg/data1/evolution'
+
+
+def csv_parquet():
     print('Starting')
 
-    csv_data = pd.read_csv("data/brokerage_monthly_money_202404221716.csv")
+    csv_data = pd.read_csv(f"{DATA_BASE}/{'csv'}/brokerage_monthly_money_202404221716.csv")
     print(csv_data)
 
-    # table = pa.Table.from_pandas(csv_data)
-    # pq.write_table(table, "data/brokerage_monthly_money_202404221716.parquet")
-    #
-    # partition_col = 'report_id'
-    # pq.write_to_dataset(table, root_path='data/partitioned_data', partition_cols=[partition_col], compression='snappy')
+    table = pa.Table.from_pandas(csv_data)
+    pq.write_table(table, f"{DATA_BASE}/{'parquet'}/brokerage_monthly_money_202404221716.parquet")
 
-    partitioned_data = pd.read_parquet('data/partitioned_data/')
+    partition_col = 'report_id'
+    pq.write_to_dataset(table, root_path=f"{DATA_BASE}/{'parquet'}/partitioned_data",
+                        partition_cols=[partition_col], compression='snappy')
+
+    partitioned_data = pd.read_parquet(f"{DATA_BASE}/{'parquet'}/partitioned_data/")
     print(partitioned_data)
 
 
+def sqlite_parquet():
+    pass
+
+
 if __name__ == "__main__":
-    routine()
+    csv_parquet()
+    sqlite_parquet()
+
